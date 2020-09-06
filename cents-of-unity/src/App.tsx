4925b1cfad71773482250b48b07cd36cc58b4ad2
Nirmit Shah <nirmitshah@lawn-128-61-36-101.lawn.gatech.edu>
2020-09-06 00:42:34 -0400

    quick eslint fixes

--- cents-of-unity/src/App.tsx
+++ cents-of-unity/src/App.tsx
@@ -1,11 +1,10 @@
 import React, { FC } from 'react'
-import { Button } from 'antd'
 import './App.css'
 import Profile from './components/profile/Profile'
 
 const App: FC = () => (
   <div className="App">
-    <Profile></Profile>
+    <Profile />
   </div>
 )
 
--- cents-of-unity/src/Components/profile/Profile.tsx
+++ cents-of-unity/src/Components/profile/Profile.tsx
@@ -1,40 +1,38 @@
 import React, { Component } from 'react'
-//import { Button } from 'antd'
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom'
 //import './Profile.css'
 import defaultImage from 'logo192.png'
 import EdiText from 'react-editext'
 
 function Image() {
-  const uploadedImage = React.useRef(null);
-  const imageUploader = React.useRef(null);
+  const uploadedImage = React.useRef(null)
+  const imageUploader = React.useRef(null)
   //const defaultImage = <img src ="logo192.png" alt =""/>
 
-  const handleImageUpload = (e : any) => {
-    const [file] = e.target.files;
+  const handleImageUpload = (e: any) => {
+    const [file] = e.target.files
     if (file) {
-      const reader = new FileReader();
+      const reader = new FileReader()
 
-      const { current } = uploadedImage;
-      
+      const { current } = uploadedImage
+
       //if (current !== undefined) {
       //current.file = file;
       //reader.onload = e => {
       //  current.src = e.target.result;
       //};
       // reader.readAsDataURL(file);
-    //}
-
+      //}
     }
-  };
+  }
 
-  return (  
+  return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center"
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
       }}
     >
       <input
@@ -43,34 +41,33 @@ function Image() {
         onChange={handleImageUpload}
         ref={imageUploader}
         style={{
-          display: "none"
+          display: 'none'
         }}
       />
       <div
         style={{
-          height: "60px",
-          width: "60px",
-          border: "1px dashed black"
+          height: '60px',
+          width: '60px',
+          border: '1px dashed black'
         }}
         //onClick={() => imageUploader.current.click()}
       >
         <img
           ref={uploadedImage}
           style={{
-            width: "100%",
-            height: "100%",
-            position: "static"
+            width: '100%',
+            height: '100%',
+            position: 'static'
           }}
-        alt = ""/>
+          alt=""
+        />
       </div>
       Click to upload Image
-
     </div>
   )
 }
 
-class Profile extends Component{
-
+class Profile extends Component {
   onSave = (val: any) => {
     console.log('Edited Value-> ', val)
   }
@@ -78,29 +75,34 @@ class Profile extends Component{
   render() {
     return (
       <div>
-  <h1> Profile for 'User'</h1>
-      <p>Name: XXXX</p> 
-      <p>Classes enrolled in:</p>
-      <li> Financial Learning Cirriculum</li>
-      <p>Contact Information:</p>
-      <li>    Phone Number:  
-      <EdiText
-      type = 'text'
-      value = 'Type your info here'
-      onSave = {this.onSave}
-      /></li>
-      <li>    Email:  
-      <EdiText
-      type = 'text'
-      value = 'Type your info here'
-      onSave = {this.onSave}
-      /></li>
+        <h1> Profile for User</h1>
+        <p>Name: XXXX </p>
+        <p>Classes enrolled in:</p>
+        <li> Financial Learning Cirriculum</li>
+        <p>Contact Information:</p>
+        <li>
+          {' '}
+          Phone Number:
+          <EdiText
+            type="text"
+            value="Type your info here"
+            onSave={this.onSave}
+          />
+        </li>
+        <li>
+          {' '}
+          Email:
+          <EdiText
+            type="text"
+            value="Type your info here"
+            onSave={this.onSave}
+          />
+        </li>
       </div>
     )
   }
-
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Profile />, rootElement);
+const rootElement = document.getElementById('root')
+ReactDOM.render(<Profile />, rootElement)
 export default Profile
