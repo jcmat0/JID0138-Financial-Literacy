4945513cac5d55b3f32125171b09ee6b6bfcedf5
Vy Tran <vytran@lawn-143-215-49-80.lawn.gatech.edu>
2020-09-26 21:53:26 -0400

    Check if user is currently logged in or not

--- cents-of-unity/src/Components/Landing/index.tsx
+++ cents-of-unity/src/Components/Landing/index.tsx
@@ -1,6 +1,21 @@
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
+import { firebase } from '../../firebase'
+import * as routes from '../../constants/routes'
 
 export const Landing = () => {
+  const [loggedIn, setLoggedIn] = useState(false)
+  useEffect(() => {
+    firebase.auth.onAuthStateChanged(function (user: any) {
+      if (user) {
+        window.location.href = routes.LANDING
+        setLoggedIn(true)
+      } else {
+        window.location.href = routes.SIGN_IN
+        setLoggedIn(false)
+      }
+    })
+  }, [loggedIn])
+
   return (
     <div>
       <h2>Landing Page</h2>
